72025205afe8ca76d48162bac8d09ac448a8bdbe
Ben V <villalobosb93@gmail.com>
2016-11-05 01:42:08 -0700

    Dashing kills enemies now. Player "dies" now

--- GDCJam/Assets/Scripts/PlayerControlScript.cs
+++ GDCJam/Assets/Scripts/PlayerControlScript.cs
@@ -26,46 +26,34 @@ public class PlayerControlScript : MonoBehaviour
         Debug.Log ("Start");
     }
 
-    void OnTriggerEnter2D (Collider2D coll)
-    {
-        if (coll.tag == "Bar") {
-            Destroy (coll.gameObject.transform.parent.gameObject);
-            Debug.Log ("Delete item");
-        }
-    }
-
 // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        
-        float translation = Time.deltaTime*10;
+        if (state != PlayerStates.Dying && state != PlayerStates.Dead) {
+            float inputX = Input.GetAxis ("Horizontal");
+            float inputY = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3(inputX, inputY, 0);
+            float translation = Time.deltaTime * 10;
 
-        if (movement != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation (Vector3.forward, movement);
+            Vector3 movement = new Vector3 (inputX, inputY, 0);
+
+            if (movement != Vector3.zero) {
+                transform.rotation = Quaternion.LookRotation (Vector3.forward, movement);
+            }
+
+            transform.position = Vector3.MoveTowards (transform.position, transform.position + movement, speed);
+
+            if (Time.time <= time + elapsedTime) {
+                transform.position = Vector3.MoveTowards (transform.position, transform.position + movement, dashSpeed);
+            } else if (state == PlayerStates.Dashing) {
+                state = PlayerStates.Normal;
+            }
+
+            if (Input.GetButtonDown ("Fire1")) {
+                time = Time.time;
+                state = PlayerStates.Dashing;
+            }
         }
-
-        transform.position = Vector3.MoveTowards (transform.position, transform.position + movement, speed);
-
-        if (Time.time <= time + elapsedTime)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, transform.position + movement, dashSpeed);
-        }
-        else if (state == PlayerStates.Dashing)
-        {
-            state = PlayerStates.Normal;
-        }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            time = Time.time;
-            state = PlayerStates.Dashing;
-        }
-        
-        Debug.Log(state.ToString());
+        Debug.Log (state.ToString ());
     }
 }
---
+++ GDCJam/Assets/Scripts/PlayerHitDetection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHitDetection : MonoBehaviour {
+
+    PlayerControlScript controlScript;
+	// Use this for initialization
+	void Start () {
+        controlScript = GetComponentInParent<PlayerControlScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        Debug.Log ("poop");
+        if (coll.tag == "Bar") {
+            if (controlScript.state == PlayerControlScript.PlayerStates.Dashing) {
+                Destroy (coll.gameObject.transform.parent.gameObject);
+            } else {
+                controlScript.state = PlayerControlScript.PlayerStates.Dying;
+            }
+        }
+    }
+}
