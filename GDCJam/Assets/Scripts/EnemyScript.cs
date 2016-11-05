1c97eaffe34853102069b0d0c5365052122f12f3
Ben V <villalobosb93@gmail.com>
2016-11-04 21:00:04 -0700

    VERY basic enemy logic done.

---
+++ GDCJam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public GameObject Player;
+    public float speed = 0.01f;
+
+	// Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards (transform.position, Player.transform.position, speed);
+        transform.Rotate (Vector3.forward * -1);
+	}
+}
