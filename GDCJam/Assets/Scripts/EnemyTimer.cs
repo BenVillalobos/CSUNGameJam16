5b801ef6343c660f30b613f562628f4838a82942
Ben V <villalobosb93@gmail.com>
2016-11-04 23:25:42 -0700

    Enemies spawn every 2 seconds EnemyTimer now has an array of spawnpoints that it chooses from randomly

--- GDCJam/Assets/Scripts/EnemyTimer.cs
+++ GDCJam/Assets/Scripts/EnemyTimer.cs
@@ -6,16 +6,27 @@ using System;
 public class EnemyTimer : MonoBehaviour {
     
     public Text TimerLabel;
-	
+    public GameObject [] SpawnPoints;
+    public GameObject BasicEnemy;
+    public GameObject player;
+    float timeGoneBy = 0;
+
     // Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
         TimerLabel.text = string.Format ("{0}", Math.Round(Time.realtimeSinceStartup,3));
+        timeGoneBy += Time.deltaTime;
+
+        if (timeGoneBy >= 2) {
+            timeGoneBy = 0;
+            GameObject newEnemy = (GameObject)Instantiate (BasicEnemy, SpawnPoints [UnityEngine.Random.Range (0, SpawnPoints.Length)].transform.position, Quaternion.identity);
+
+            EnemyScript newScript = newEnemy.GetComponent<EnemyScript> ();
+            newScript.Player = player;
+        }
 	}
-
-
 }
