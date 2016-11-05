19cad7ec40bc392e3e8266b41f1bf83dc940e911
Ben V <villalobosb93@gmail.com>
2016-11-04 19:32:48 -0700

    Added my own scene for test development.
    Got a basic timer on screen.

---
+++ GDCJam/Assets/Scripts/EnemyTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class EnemyTimer : MonoBehaviour {
+    
+    public Text TimerLabel;
+	
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimerLabel.text = string.Format ("{0}", Math.Round(Time.realtimeSinceStartup,3));
+	}
+
+
+}
