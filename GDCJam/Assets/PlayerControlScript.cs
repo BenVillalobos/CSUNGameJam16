d23120e4963ef26fabdc7953d72c01968e27c80b
ArmandAbrahamian <armand.abrahamian@gmail.com>
2016-11-04 21:00:37 -0700

    Added Player Controller Script and x y axis movement using PC and XBOX One controls

---
+++ GDCJam/Assets/PlayerControlScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+   //     movementVector.x = Input.GetAxis("LeftJoystickX")*movementSpeed;
+   //     movementVector.z = Input.GetAxis("LeftJoystickY")*movementSpeed;
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+
+}
