1705489d34184112d7c1ce26d35d629b82f65b7e
ArmandAbrahamian <armand.abrahamian@gmail.com>
2016-11-04 23:28:23 -0700

    In the process of adding timed dash for player

--- GDCJam/Assets/Scripts/PlayerControlScript.cs
+++ GDCJam/Assets/Scripts/PlayerControlScript.cs
@@ -3,29 +3,39 @@ using System.Collections;
 
 public class PlayerControlScript : MonoBehaviour
 {
-    public float speed = 0.3f;
-    int number = 0;
+    public float speed = 0.1f;
+    private float time;
+
     // Use this for initialization
     void Start()
     {
+        time = Time.time;
     }
 
 // Update is called once per frame
     void Update()
     {
-   //     movementVector.x = Input.GetAxis("LeftJoystickX")*movementSpeed;
-   //     movementVector.z = Input.GetAxis("LeftJoystickY")*movementSpeed;
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
+        float dashSpeed = 0.2f;
+        float translation = Time.deltaTime*10;
 
         Vector3 movement = new Vector3(inputX, inputY, 0);
 
-        if (movement != Vector3.zero) {
+        if (movement != Vector3.zero)
+        {
             transform.rotation = Quaternion.LookRotation (Vector3.forward, movement);
         }
 
-
         transform.position = Vector3.MoveTowards (transform.position, transform.position + movement, speed);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time >= time + 1f)
+            {
+                //code something
+                time = Time.time;
+            }
     }
 
 }
