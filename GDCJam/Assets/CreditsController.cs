437adfb1e863b5d3f011f9e8f98e14802956cbb0
Armand Abrahamian <armand.abrahamian.987@my.csun.edu>
2016-11-06 17:04:17 -0800

    Added back button functionality to credits.

---
+++ GDCJam/Assets/CreditsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BackButtonPressed()
+    {
+        SceneManager.LoadScene("Title", LoadSceneMode.Single);
+    }
+}
